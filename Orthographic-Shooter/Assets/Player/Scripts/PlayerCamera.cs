b55909e2e2753871b9dfab33bdc1b055f642037b
Trottero <wrlxs@hotmail.com>
2021-06-06 15:26:26 +0200

    Start on making objects transparant when behind objects

--- Orthographic-Shooter/Assets/Player/Scripts/PlayerCamera.cs
+++ Orthographic-Shooter/Assets/Player/Scripts/PlayerCamera.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Game.Behaviours;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    public Transform player;
+    public Transform Player;
 
     private Vector3 _cameraPosition;
     private Quaternion CameraRotation;
@@ -18,10 +19,30 @@ public class PlayerCamera : MonoBehaviour
 
     void Update()
     {
+        // Make objects transparant when the Player is behind them.
+        // Cast ray from camera centre to ray.
+        var playerDirection = Player.position - transform.position;
+        Debug.DrawLine(transform.position, Player.position);
+        if (Physics.Raycast(transform.position, playerDirection.normalized, out var hitInfo, playerDirection.magnitude - 1, -1))
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+            if (isHitValid(hitInfo))
+            {
 
+            }
+        }
     }
     void LateUpdate()
     {
-        transform.position = new Vector3(player.position.x, 0, player.position.z) + _cameraPosition;
+        transform.position = new Vector3(Player.position.x, 0, Player.position.z) + _cameraPosition;
+    }
+
+    bool isHitValid(RaycastHit hit)
+    {
+        if (hit.collider.gameObject.GetComponent<IgnoreCameraVisibility>() != null)
+        {
+            return false;
+        }
+        return true;
     }
 }
